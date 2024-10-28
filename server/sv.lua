c8300847f8c666086a886ea884a5291522ac02c3
xalux <xaluxscripting@gmail.com>
2024-10-28 21:32:11 +0200

    Update sv.lua

--- server/sv.lua
+++ server/sv.lua
@@ -1,107 +1,161 @@
-Config = Config or {} 
+Config = Config or {}
 
-
-local DISCORD_WEBHOOK_URL = Config.Webhook 
-
-
+local webhook = Config.Webhook
+local botToken = Config.BotToken 
 local playerLocations = {}
+
 
 local function generateUniqueWarehouseId()
     local warehouseId, isUnique = nil, false
     while not isUnique do
         warehouseId = math.random(500, 1000)
         local result = MySQL.query.await('SELECT `warehouse_id` FROM `warehouses` WHERE `warehouse_id` = ?', {warehouseId})
-        if result[1] == nil then isUnique = true end 
+        if result[1] == nil then isUnique = true end
     end
     return warehouseId
 end
 
 
-local function getPlayerIdentifiers(src)
-    local discordIdentifier = nil
+local function getSteamIdentifier(src)
     for _, id in ipairs(GetPlayerIdentifiers(src)) do
-        if string.match(id, "discord:") then
-            discordIdentifier = id
+        if string.match(id, "steam:") then
+            return id
         end
     end
+    return nil
+end
 
-    return discordIdentifier
+
+local function getDiscordIdentifierAndTag(src, callback)
+    local discordId
+    for _, id in ipairs(GetPlayerIdentifiers(src)) do
+        if string.match(id, "discord:") then
+            discordId = string.sub(id, 9)
+            break
+        end
+    end
+    if discordId then
+        PerformHttpRequest("https://discord.com/api/v10/users/" .. discordId, function(err, response, headers)
+            if err == 200 then
+                local data = json.decode(response)
+                callback(discordId, data.username .. "#" .. data.discriminator)
+            else
+                print("Error fetching Discord tag for ID: " .. discordId)
+                callback(discordId, nil)
+            end
+        end, "GET", "", {["Authorization"] = "Bot " .. botToken})
+    else
+        callback(nil, nil)
+    end
 end
 
 
 local function sendToDiscord(title, message, color)
-    PerformHttpRequest(DISCORD_WEBHOOK_URL, function(err, text, headers) end, 'POST', json.encode({
-        embeds = { {
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({
+        embeds = {{
             title = title,
             description = message,
             color = color
         }}
-    }), { ['Content-Type'] = 'application/json' })
+    }), {['Content-Type'] = 'application/json'})
 end
 
+local function getDiscordIdentifierAndTag(src, callback)
+    local discordId
+    for _, id in ipairs(GetPlayerIdentifiers(src)) do
+        if string.match(id, "discord:") then
+            discordId = string.sub(id, 9)
+            break
+        end
+    end
+    if discordId then
+        PerformHttpRequest("https://discord.com/api/v10/users/" .. discordId, function(err, response, headers)
+            if err == 200 then
+                local data = json.decode(response)
+                callback(discordId, data.username .. "#" .. data.discriminator)
+            else
+                print("Error fetching Discord tag for ID: " .. discordId)
+                callback(discordId, nil)
+            end
+        end, "GET", "", {["Authorization"] = "Bot " .. botToken})
+    else
+        callback(nil, nil)
+    end
+end
+
+local function sendToDiscord(title, message, color)
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({
+        embeds = {{
+            title = title,
+            description = message,
+            color = color
+        }}
+    }), {['Content-Type'] = 'application/json'})
+end
 
 RegisterNetEvent('warehouse:buy')
 AddEventHandler('warehouse:buy', function(warehouseIndex, warehouseName, warehouseCode)
     local src = source
     local warehouse = Config.Warehouses[warehouseIndex]
-    local discordId = getPlayerIdentifiers(src)
-    local playerName = GetPlayerName(src) 
+    local steamId = getSteamIdentifier(src)
+    local playerName = GetPlayerName(src)
 
+    getDiscordIdentifierAndTag(src, function(discordId, discordTag)
+        if not warehouseName or warehouseName == "" then
+            TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Invalid warehouse name!', type = 'error'})
+            return
+        end
 
-    if not warehouseName or warehouseName == "" then
-        TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Invalid warehouse name!', type = 'error'})
-        return
-    end
+        if not warehouseCode or warehouseCode == "" or #warehouseCode ~= 4 then
+            TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Invalid access code! Must be 4 digits.', type = 'error'})
+            return
+        end
 
-    if not warehouseCode or warehouseCode == "" or #warehouseCode ~= 4 then
-        TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Invalid access code! Must be 4 digits.', type = 'error'})
-        return
-    end
+        local existingWarehouse = MySQL.query.await('SELECT `name` FROM `warehouses` WHERE `name` = ?', {warehouseName})
+        if existingWarehouse[1] then
+            TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'A warehouse with this name already exists!', type = 'error'})
+            return
+        end
 
-    local existingWarehouse = MySQL.query.await('SELECT `name` FROM `warehouses` WHERE `name` = ?', {warehouseName})
-    if existingWarehouse[1] then
-        TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'A warehouse with this name already exists!', type = 'error'})
-        return
-    end
+        local money = exports.ox_inventory:GetItem(src, 'money')
+        local warehousePrice = warehouse.price
+        if not money or money.count < warehousePrice then
+            TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Insufficient money to buy this warehouse!', type = 'error'})
+            return
+        end
 
-    local money = exports.ox_inventory:GetItem(src, 'money')
-    local warehousePrice = warehouse.price
-    if not money or money.count < warehousePrice then
-        TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Insufficient money to buy this warehouse!', type = 'error'})
-        return
-    end
+        exports.ox_inventory:RemoveItem(src, 'money', warehousePrice)
+        local warehouseId = generateUniqueWarehouseId()
 
+        MySQL.insert.await('INSERT INTO `warehouses` (owner, steam_id, name, code, location, warehouse_id, entry_coords, max_slots, max_weight, discord, original_price) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', {
+            playerName, steamId, warehouseName, warehouseCode, json.encode(warehouse.coords), warehouseId, json.encode(warehouse.coords), Config.stashes.defaultSlots, Config.stashes.defaultWeight, discordId, warehousePrice
+        })
 
-    exports.ox_inventory:RemoveItem(src, 'money', warehousePrice)
+        exports.ox_inventory:RegisterStash('warehouse_' .. warehouseId, warehouseName, Config.stashes.defaultSlots, Config.stashes.defaultWeight, playerName)
 
-    local warehouseId = generateUniqueWarehouseId()
+        TriggerClientEvent('ox_lib:notify', src, {title = 'Success', description = 'You bought the warehouse: ' .. warehouseName, type = 'success'})
+        TriggerClientEvent('warehouse:setupStashTarget', src, warehouse.coords, warehouseId)
 
-    MySQL.insert.await('INSERT INTO `warehouses` (owner, discord, name, code, location, warehouse_id, entry_coords) VALUES (?, ?, ?, ?, ?, ?, ?)', {
-        playerName, discordId, warehouseName, warehouseCode, json.encode(warehouse.coords), warehouseId, json.encode(warehouse.coords)
-    })
-
-
-    exports.ox_inventory:RegisterStash('warehouse_' .. warehouseId, warehouseName, Config.stashes.slots, Config.stashes.maxWeight, playerName)
-
-    TriggerClientEvent('ox_lib:notify', src, {title = 'Success', description = 'You bought the warehouse: ' .. warehouseName, type = 'success'})
-
-    TriggerClientEvent('warehouse:setupStashTarget', src, warehouse.coords, warehouseId)
-
-    sendToDiscord("Warehouse Purchased", ("**Player:** %s\n**Discord:** %s\n**Warehouse:** %s\n**Price:** $%d"):format(playerName, discordId, warehouseName, warehousePrice), 3066993)
+        local discordDisplay = discordTag or "Unknown"
+        local mention = discordId and ("<@" .. discordId .. ">") or "Unknown"
+        
+        sendToDiscord("Warehouse Purchase", ("**Player Steam Name:** %s\n**Discord:** %s\n**Warehouse Name:** %s\n**Price:** $%d"):format(playerName, mention, warehouseName, warehousePrice), 3447003)
+    end)
 end)
+
 
 RegisterNetEvent('warehouse:changePin')
 AddEventHandler('warehouse:changePin', function(warehouseId, newCode)
     local src = source
-    local playerName = GetPlayerName(src)
+    local steamId = getSteamIdentifier(src)
 
-    -- Validate the new code
+
     if not newCode or #newCode ~= 4 then
         TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Invalid access code! Must be 4 digits.', type = 'error'})
         return
     end
 
-    MySQL.update('UPDATE `warehouses` SET `code` = ? WHERE `warehouse_id` = ? AND `owner` = ?', {newCode, warehouseId, playerName}, function(affectedRows)
+    MySQL.update('UPDATE `warehouses` SET `code` = ? WHERE `warehouse_id` = ? AND `steam_id` = ?', {newCode, warehouseId, steamId}, function(affectedRows)
         if affectedRows > 0 then
             TriggerClientEvent('ox_lib:notify', src, {title = 'Success', description = 'Warehouse pincode changed successfully!', type = 'success'})
         else
@@ -109,6 +163,39 @@ AddEventHandler('warehouse:changePin', function(warehouseId, newCode)
         end
     end)
 end)
+
+
+RegisterNetEvent('warehouse:upgradeStash')
+AddEventHandler('warehouse:upgradeStash', function(warehouseId, upgradeType, upgradeAmount, upgradeCost)
+    local src = source
+    local playerName = GetPlayerName(src)
+
+    getDiscordIdentifierAndTag(src, function(discordId, discordTag)
+        local mention = discordId and ("<@" .. discordId .. ">") or "Unknown"
+
+        local money = exports.ox_inventory:GetItem(src, 'money')
+        if not money or money.count < upgradeCost then
+            TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Insufficient funds for this upgrade!', type = 'error'})
+            return
+        end
+
+        exports.ox_inventory:RemoveItem(src, 'money', upgradeCost)
+
+        if upgradeType == "slots" then
+            MySQL.update.await('UPDATE `warehouses` SET `max_slots` = `max_slots` + ? WHERE `warehouse_id` = ?', {upgradeAmount, warehouseId})
+        elseif upgradeType == "weight" then
+            MySQL.update.await('UPDATE `warehouses` SET `max_weight` = `max_weight` + ? WHERE `warehouse_id` = ?', {upgradeAmount * 1000, warehouseId}) 
+        end
+
+        local warehouse = MySQL.query.await('SELECT `name`, `max_slots`, `max_weight` FROM `warehouses` WHERE `warehouse_id` = ?', {warehouseId})
+        exports.ox_inventory:RegisterStash('warehouse_' .. warehouseId, warehouse[1].name, warehouse[1].max_slots, warehouse[1].max_weight, playerName)
+
+        TriggerClientEvent('ox_lib:notify', src, {title = 'Success', description = 'Warehouse upgraded successfully!', type = 'success'})
+
+        sendToDiscord("Warehouse Upgrade", ("**Player:** %s\n**Warehouse ID:** %d\n**Discord:** %s\n**Upgrade Type:** %s\n**Upgrade Amount:** %d\n**Cost:** $%d"):format(playerName, warehouseId, mention, upgradeType, upgradeAmount, upgradeCost), 3066993)
+    end)
+end)
+
 
 RegisterNetEvent('warehouse:enter')
 AddEventHandler('warehouse:enter', function(warehouseName, enteredCode, playerCoords)
@@ -122,6 +209,7 @@ AddEventHandler('warehouse:enter', function(warehouseName, enteredCode, playerCo
                 local isOwner = (result[1].owner == GetPlayerName(src))
 
                 playerLocations[src] = playerCoords
+
 
                 SetPlayerRoutingBucket(src, warehouseId)
                 TriggerClientEvent('warehouse:teleportInside', src, warehouseId, isOwner)
@@ -137,14 +225,16 @@ AddEventHandler('warehouse:enter', function(warehouseName, enteredCode, playerCo
 end)
 
 RegisterNetEvent('warehouse:leave')
-AddEventHandler('warehouse:leave', function()
+AddEventHandler('warehouse:leave', function(src)
     local src = source
-    local originalPos = playerLocations[src]
+    local originalPos = playerLocations[src]  
 
     SetPlayerRoutingBucket(src, 0)
     if originalPos then
         TriggerClientEvent('warehouse:teleportOutside', src, originalPos)
-        playerLocations[src] = nil
+        playerLocations[src] = nil  
+    else
+        TriggerClientEvent('ox_lib:notify', src, {title = 'Notice', description = 'No saved exit location found!', type = 'error'})
     end
 end)
 
@@ -160,38 +250,24 @@ RegisterCommand("requestStashInfo", function(source, args, rawCommand)
 
         local warehouseName = args[1]
 
-        MySQL.query('SELECT `warehouse_id`, `owner`, `discord` FROM `warehouses` WHERE `name` = ?', {warehouseName}, function(result)
+        MySQL.query('SELECT `warehouse_id`, `owner`, `discord`, `max_slots`, `max_weight` FROM `warehouses` WHERE `name` = ?', {warehouseName}, function(result)
             if result[1] then
                 local warehouseId = result[1].warehouse_id
-                local ownerSteamId = result[1].owner
-                local ownerDiscord = result[1].discord
+                local ownerName = result[1].owner
+                local ownerDiscord = result[1].discord and ("<@" .. result[1].discord .. ">") or "Unknown"
                 local stashId = 'warehouse_' .. warehouseId
-                local ownerSteamName = nil 
-
-                for _, playerId in ipairs(GetPlayers()) do
-                    if GetPlayerIdentifier(playerId, 0) == ownerSteamId then
-                        ownerSteamName = GetPlayerName(playerId)
-                        break
-                    end
-                end
-
-                if not ownerSteamName then
-                    ownerSteamName = "Owner is offline (Steam ID: " .. ownerSteamId .. ")"
-                end
-
                 local inventory = exports.ox_inventory:GetInventory(stashId)
                 if inventory and inventory.items then
-                    local stashItems = inventory.items
                     local itemList = ""
-                    for _, item in pairs(stashItems) do
+                    for _, item in pairs(inventory.items) do
                         itemList = itemList .. ("**Item:** %s | **Count:** %d\n"):format(item.name, item.count)
                     end
 
                     if itemList == "" then
                         itemList = "The stash is empty."
                     end
-                    sendToDiscord("Warehouse Stash Info", ("**Warehouse Name:** %s\n**Owner:** %s\n**Owner Discord:** %s\n**Requested by:** %s\n\n%s"):format(warehouseName, ownerSteamName, ownerDiscord, playerName, itemList), 3447003)
 
+                    sendToDiscord("Warehouse Stash Info", ("**Warehouse Name:** %s\n**Owner:** %s\n**Owner Discord:** %s\n**Requested by:** %s\n\n%s"):format(warehouseName, ownerName, ownerDiscord, playerName, itemList), 3447003)
                     TriggerClientEvent('ox_lib:notify', src, {title = 'Success', description = 'Stash info sent to Discord!', type = 'success'})
                 else
                     TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'No items found in the stash.', type = 'error'})
@@ -206,14 +282,57 @@ RegisterCommand("requestStashInfo", function(source, args, rawCommand)
 end)
 
 
+
+
+
 AddEventHandler('onResourceStart', function(resourceName)
-    if GetCurrentResourceName() ~= resourceName then
-        return
-    end
-    local warehouses = MySQL.query.await('SELECT `warehouse_id`, `name`, `owner` FROM `warehouses`')
+    if GetCurrentResourceName() ~= resourceName then return end
+    local warehouses = MySQL.query.await('SELECT `warehouse_id`, `name`, `owner`, `max_slots`, `max_weight` FROM `warehouses`')
     if warehouses then
         for _, warehouse in pairs(warehouses) do
-            exports.ox_inventory:RegisterStash('warehouse_' .. warehouse.warehouse_id, warehouse.name, Config.stashes.slots, Config.stashes.maxWeight, warehouse.owner)
+            exports.ox_inventory:RegisterStash('warehouse_' .. warehouse.warehouse_id, warehouse.name, warehouse.max_slots, warehouse.max_weight, warehouse.owner)
         end
     end
 end)
+
+
+RegisterNetEvent('warehouse:sell')
+AddEventHandler('warehouse:sell', function()
+    local src = source
+    local steamId = getSteamIdentifier(src)
+    local playerName = GetPlayerName(src)
+    local warehouseId = GetPlayerRoutingBucket(src) 
+    local saleCutPercentage = Config.sellpros
+
+    getDiscordIdentifierAndTag(src, function(discordId, discordTag)
+        local mention = discordId and ("<@" .. discordId .. ">") or "Unknown"
+
+        local warehouse = MySQL.query.await('SELECT `name`, `original_price`, `steam_id` FROM `warehouses` WHERE `warehouse_id` = ?', {warehouseId})
+        if not warehouse[1] or tostring(warehouse[1].steam_id) ~= tostring(steamId) then
+            TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'You are not the owner of this warehouse!', type = 'error'})
+            return
+        end
+
+        local warehousePrice = warehouse[1].original_price or 0
+        local payoutAmount = math.floor(warehousePrice * (1 - saleCutPercentage))
+
+        if payoutAmount > 0 then
+            exports.ox_inventory:AddItem(src, 'money', payoutAmount)
+        else
+            TriggerClientEvent('ox_lib:notify', src, {title = 'Error', description = 'Calculated payout is invalid!', type = 'error'})
+            return
+        end
+
+        exports.ox_inventory:ClearInventory('warehouse_' .. warehouseId)
+        MySQL.query.await('DELETE FROM `warehouses` WHERE `warehouse_id` = ?', {warehouseId})
+
+        TriggerClientEvent('ox_lib:notify', src, {title = 'Success', description = ('You sold the warehouse for $%d after a %.0f%% cut.'):format(payoutAmount, saleCutPercentage * 100), type = 'success'})
+
+        sendToDiscord("Warehouse Sold", ("**Player:** %s\n**Discord:** %s\n**Warehouse Name:** %s\n**Original Price:** $%d\n**Payout Amount:** $%d\n")
+            :format(playerName, mention, warehouse[1].name, warehousePrice, payoutAmount), 16056320)
+
+        TriggerEvent('warehouse:leave', src)
+    end)
+end)
+
+
